7c70bb6cbf225c69fe148589ace15ba1ad130b62
Wafiq Muhammad Abdul Aziz <caris.canismajoris@gmail.com>
2024-04-01 10:55:15 +0700

    command

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,7 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+//turnoff features
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
